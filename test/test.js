07f51355c282e952833d4cb0d65797c72801cba4
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-09-19 02:26:40 +0200

    Amélioration des tests

--- test/test.js
+++ test/test.js
@@ -53,15 +53,11 @@ test("getProcessMemory()", async function getProcessMemory(assert) {
 
     for (const [processName, processMemory] of Object.entries(processMemories)) {
         assert.is(is.string(processName), true);
+        assert.is(is.number(processMemory.processId), true);
         assert.is(is.plainObject(processMemory), true);
 
         assert.is(Reflect.has(processMemory, "error"), true);
-        if (!is.string(processMemory.error) && !is.nullOrUndefined(processMemory.error)) {
-            assert.fail();
-        }
-        assert.is(is.number(processMemory.processId), true);
-
-        if (is.string(processMemory.error)) {
+        if (is.string(processMemory.error) || !is.nullOrUndefined(processMemory.error)) {
             assert.is(processMemory.pageFaultCount, 0);
             assert.is(processMemory.peakWorkingSetSize, 0);
             assert.is(processMemory.workingSetSize, 0);
@@ -73,7 +69,7 @@ test("getProcessMemory()", async function getProcessMemory(assert) {
             assert.is(processMemory.peakPagefileUsage, 0);
             assert.is(processMemory.privateUsage, 0);
         }
-        else {
+        else if (is.nullOrUndefined(processMemory.error)) {
             assert.is(is.number(processMemory.pageFaultCount), true);
             assert.is(is.number(processMemory.peakWorkingSetSize), true);
             assert.is(is.number(processMemory.workingSetSize), true);
@@ -84,6 +80,9 @@ test("getProcessMemory()", async function getProcessMemory(assert) {
             assert.is(is.number(processMemory.pagefileUsage), true);
             assert.is(is.number(processMemory.peakPagefileUsage), true);
             assert.is(is.number(processMemory.privateUsage), true);
+        }
+        else {
+            assert.fail("Invalid type for error field. It should be either string or null.");
         }
     }
 });
